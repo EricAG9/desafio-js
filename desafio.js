2538e45268d959c5e8dcf1b295ab6af880dd9e78
Eric Galvao <98236307+EricAG9@users.noreply.github.com>
2023-11-06 20:11:16 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -3,6 +3,6 @@ estrela2 = 15,
 estrela3 = 18,
 estrela4 = 25,
 estrela5 = 40,
- media = (estrela*1+estrela2*2+estrela3*3+estrela4*4+estrela5*5)/100
+ media = ((estrela * 1)+(estrela2 * 2)+(estrela3 * 3)+(estrela4 * 4)+(estrela5 * 5))/100
 
-console.log(media)
+console.log("A média foi de: " + media)
